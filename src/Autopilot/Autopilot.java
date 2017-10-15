a0d1e55aedfe703b2f1eb74e4d085776ce62bb79
BartJacobs1 <32458828+BartJacobs1@users.noreply.github.com>
2017-10-15 17:49:24 +0200

    Autopilot files

---
+++ src/Autopilot/Autopilot.java
@@ -0,0 +1,9 @@
+package Autopilot;
+
+public interface Autopilot {
+    
+	AutopilotOutputs simulationStarted(AutopilotConfig config, AutopilotInputs inputs);
+	
+    AutopilotOutputs timePassed(AutopilotInputs inputs);
+    void simulationEnded();
+}
