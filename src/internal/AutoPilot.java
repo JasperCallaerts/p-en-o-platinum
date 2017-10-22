e604d3e55654009bb2feaa001dae9e3acad51a1b
Anthony Rathé <anthonyrathe@gmail.com>
2017-10-22 18:24:04 +0200

    AP updates

--- src/internal/AutoPilot.java
+++ src/internal/AutoPilot.java
@@ -6,12 +6,15 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import Autopilot.*;
 
 /**
  * Created by Martijn on 14/10/2017.
  * Extended by Bart on 15/10/2017.
+ * Extended by Anthony Rathé on 16/10/2017 and later
  */
 public class AutoPilot implements Autopilot{
 
@@ -118,85 +121,88 @@ public class AutoPilot implements Autopilot{
 	private float horStabInclination;
 	private float verStabInclination;
 	
+	private List<Vector> currentPath;
+	private AutoPilotCamera APCamera;
+	
 	//------- Simple Controlling Methods -------
 	/**
 	 * @author anthonyrathe
 	 */
 	private void clockRollStart(){
-		this.setLeftWingInclination((float)-Math.PI/2);
-		this.setRightWingInclination((float)Math.PI/2);
+		this.setLeftWingInclinationOut((float)-STANDARD_INCLINATION);
+		this.setRightWingInclinationOut((float)STANDARD_INCLINATION);
 	}
 	
 	/**
 	 * @author anthonyrathe
 	 */
 	private void counterClockRollStart(){
-		this.setLeftWingInclination((float)Math.PI/2);
-		this.setRightWingInclination((float)-Math.PI/2);
+		this.setLeftWingInclinationOut((float)STANDARD_INCLINATION);
+		this.setRightWingInclinationOut((float)-STANDARD_INCLINATION);
 	}
 	
 	/**
 	 * @author anthonyrathe
 	 */
 	private void stopRoll(){
-		this.setLeftWingInclination((float)0);
-		this.setRightWingInclination((float)0);
+		this.setLeftWingInclinationOut((float)0);
+		this.setRightWingInclinationOut((float)0);
 	}
 	
 	/**
 	 * @author anthonyrathe
 	 */
 	private void startTurnLeft(){
-		this.setVerStabInclination((float)Math.PI/2);
+		this.setVerStabInclinationOut((float)STANDARD_INCLINATION);
 	}
 	
 	/**
 	 * @author anthonyrathe
 	 */
 	private void startTurnRight(){
-		this.setVerStabInclination((float)-Math.PI/2);
+		this.setVerStabInclinationOut((float)-STANDARD_INCLINATION);
 	}
 	
 	/**
 	 * @author anthonyrathe
 	 */
 	private void stopTurn(){
-		this.setVerStabInclination(0f);
+		this.setVerStabInclinationOut(0f);
 	}
 	
 	/**
 	 * @author anthonyrathe
 	 */
 	private void startAscend(){
-		this.setHorStabInclination((float)Math.PI/2);
+		this.setHorStabInclinationOut((float)STANDARD_INCLINATION);
 	}
 	
 	/**
 	 * @author anthonyrathe
 	 */
 	private void startDescend(){
-		this.setHorStabInclination((float)-Math.PI/2);
+		this.setHorStabInclinationOut((float)-STANDARD_INCLINATION);
 	}
 	
 	/**
 	 * @author anthonyrathe
 	 */
 	private void stopAscendDescend(){
-		this.setHorStabInclination(0f);
+		this.setHorStabInclinationOut(0f);
 	}
 	
 	/**
 	 * @author anthonyrathe
 	 */
 	private void thrustOn(){
-		this.setThrust(1);
+		this.setThrustOut(1);
 	}
 	
 	/**
 	 * @author anthonyrathe
 	 */
 	private void thrustOff(){
-		this.setThrust(0);
+		this.setThrustOut(0);
 	}
 	
 	//------- END Drone Controlling Methods -------
@@ -205,10 +211,10 @@ public class AutoPilot implements Autopilot{
 	/**
 	 * @author anthonyrathe
 	 */
-	private void updatePath(){
+	private void updatePath() throws IOException{
 		int[] start = this.getPosition().toIntArray();
 		int[] end = this.getDestinationPosition().toIntArray();
-		List<int[]> pathInt = getPathfinding().searchPath(start, end);
+		List<int[]> pathInt = Pathfinding.searchPath(start, end);
 		List<Vector> newPath = new ArrayList<Vector>();
 		for (int[] position : pathInt){
 			newPath.add(new Vector(position[0], position[1], position[2]));
@@ -234,6 +240,23 @@ public class AutoPilot implements Autopilot{
 	
 	/**
 	 * @author anthonyrathe
+	 */
+	private AutoPilotCamera getAPCamera() throws NullPointerException{
+		if (this.APCamera == null){
+			throw new NullPointerException("No APCamera was assigned to this AutoPilot");
+		}
+		return this.APCamera;
+	}
+	
+	/**
+	 * @author anthonyrathe
+	 */
+	private void loadAPCamera(AutoPilotCamera newAPCamera){
+		this.APCamera = newAPCamera;
+	}
+	
+	/**
+	 * @author anthonyrathe
 	 * @return
 	 */
 	private Vector getDestinationPosition(){
@@ -254,7 +277,7 @@ public class AutoPilot implements Autopilot{
 	 * @author anthonyrathe
 	 * @return the node that currently is closest to the drone
 	 */
-	private Vector getNextNode(){
+	private Vector getNextNode() throws IOException{
 		updatePath();
 		Vector nextNode = getDestinationPosition();
 		float smallestDistance = this.getPosition().distanceBetween(getDestinationPosition());
@@ -278,14 +301,43 @@ public class AutoPilot implements Autopilot{
 	 * @author anthonyrathe
 	 */
 	//TODO Max angle of attack error uitwerken
-	public void update(){
-		Vector longitudinalAxis = Vector();
-		Vector perpendicularAxis = Vector();
-		Vector lateralAxis = Vector();
+	public void update() throws IOException{
+
+		Vector perpendicularAxis = Vector(); //pointed to the roof of the drone
+		Vector lateralAxis = Vector(); //pointed to the left of the drone
 		
 		Vector directionToNode = getPosition().vectorDifference(getNextNode());
+		float verticalAngle = perpendicularAxis.getAngleBetween(directionToNode);
+		float horizontalAngle = lateralAxis.getAngleBetween(directionToNode);
 		
-		float horizontalAngle = longitudinal
+		// Ascend/Descend
+		if (verticalAngle > Math.PI){
+			throw new IOException("Undefined angles");
+		}else if(verticalAngle > Math.PI + THRESHOLD_ANGLE && verticalAngle <= Math.PI){
+			// Descend
+			this.startDescend();
+		}else if(verticalAngle <= Math.PI + THRESHOLD_ANGLE && verticalAngle >= Math.PI - THRESHOLD_ANGLE){
+			// Stop descending/ascending
+			this.stopAscendDescend();
+		}else if(verticalAngle >= 0f && verticalAngle < Math.PI - THRESHOLD_ANGLE){
+			// Ascend
+			this.startAscend();
+		}
+		
+		// Roll
+		if (horizontalAngle > Math.PI){
+			throw new IOException("Undefined angles");
+		}else if(horizontalAngle > Math.PI + THRESHOLD_ANGLE && horizontalAngle <= Math.PI){
+			// Roll clockwise
+			this.clockRollStart();
+		}else if(horizontalAngle <= Math.PI + THRESHOLD_ANGLE && horizontalAngle >= Math.PI - THRESHOLD_ANGLE){
+			// Stop rolling
+			this.stopRoll();
+		}else if(horizontalAngle >= 0f && horizontalAngle < Math.PI - THRESHOLD_ANGLE){
+			// Roll counterclockwise
+			this.counterClockRollStart();
+		} 
+		
 	}
 	
 	
@@ -329,6 +381,10 @@ public class AutoPilot implements Autopilot{
 	public void setVerStabInclinationOut(float inclination){
 		verStabInclination = inclination;
 	}
+	
+	//------- Parameters -------
+	private static final float STANDARD_INCLINATION = (float)Math.PI/3;
+	private static final float THRESHOLD_ANGLE = (float)Math.PI/36;
 
 }
 
