aad7c9aefa95bf9dc08017b502396ffb30363092
Jasper <jasper.snoeks@kuleuven.be>
2017-10-23 18:39:36 +0200

    gui???

---
+++ bin/README.md
@@ -0,0 +1,1 @@
+# p-en-o-platinum
---
+++ src/gui/Mesh.java
@@ -0,0 +1,97 @@
+package gui;
+
+import static org.lwjgl.opengl.GL11.GL_FLOAT;
+import static org.lwjgl.opengl.GL11.GL_TRIANGLES;
+import static org.lwjgl.opengl.GL11.GL_UNSIGNED_INT;
+import static org.lwjgl.opengl.GL11.glDrawElements;
+import static org.lwjgl.opengl.GL15.GL_ARRAY_BUFFER;
+import static org.lwjgl.opengl.GL15.GL_ELEMENT_ARRAY_BUFFER;
+import static org.lwjgl.opengl.GL15.GL_STATIC_DRAW;
+import static org.lwjgl.opengl.GL15.glBindBuffer;
+import static org.lwjgl.opengl.GL15.glBufferData;
+import static org.lwjgl.opengl.GL15.glDeleteBuffers;
+import static org.lwjgl.opengl.GL15.glGenBuffers;
+import static org.lwjgl.opengl.GL20.glDisableVertexAttribArray;
+import static org.lwjgl.opengl.GL20.glEnableVertexAttribArray;
+import static org.lwjgl.opengl.GL20.glVertexAttribPointer;
+import static org.lwjgl.opengl.GL30.glBindVertexArray;
+import static org.lwjgl.opengl.GL30.glDeleteVertexArrays;
+import static org.lwjgl.opengl.GL30.glGenVertexArrays;
+
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
+
+import org.lwjgl.system.MemoryUtil;
+
+public class Mesh {
+	private int vao;
+	private int posvbo;
+	private int colvbo;
+	private int indvbo;
+	private int vertexCount;
+
+	public void init(float[] positions, float[] colours, int[] indices) { 
+		FloatBuffer posBuffer = null;
+        FloatBuffer colourBuffer = null;
+        IntBuffer indicesBuffer = null;
+		
+		vertexCount = indices.length;
+		
+        vao = glGenVertexArrays();
+        glBindVertexArray(vao);
+        
+        // Position VBO
+        posvbo = glGenBuffers();
+        posBuffer = MemoryUtil.memAllocFloat(positions.length);
+        posBuffer.put(positions).flip();
+        glBindBuffer(GL_ARRAY_BUFFER, posvbo);
+        glBufferData(GL_ARRAY_BUFFER, posBuffer, GL_STATIC_DRAW);
+        glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, 0);
+
+        // Colour VBO
+        colvbo = glGenBuffers();
+        colourBuffer = MemoryUtil.memAllocFloat(colours.length);
+        colourBuffer.put(colours).flip();
+        glBindBuffer(GL_ARRAY_BUFFER, colvbo);
+        glBufferData(GL_ARRAY_BUFFER, colourBuffer, GL_STATIC_DRAW);
+        glVertexAttribPointer(1, 3, GL_FLOAT, false, 0, 0);
+
+        // Index VBO
+        indvbo = glGenBuffers();
+        indicesBuffer = MemoryUtil.memAllocInt(indices.length);
+        indicesBuffer.put(indices).flip();
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indvbo);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indicesBuffer, GL_STATIC_DRAW);
+        
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glBindVertexArray(0);
+	}
+	
+	public void render() {
+		// Draw the mesh
+        glBindVertexArray(vao);
+        glEnableVertexAttribArray(0);
+        glEnableVertexAttribArray(1);
+
+        glDrawElements(GL_TRIANGLES, vertexCount, GL_UNSIGNED_INT, 0);
+
+        // Restore state
+        glDisableVertexAttribArray(0);
+        glDisableVertexAttribArray(1);
+        glBindVertexArray(0);
+	}
+
+	public void delete() {
+		glDisableVertexAttribArray(0);
+
+        // Delete the VBOs
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glDeleteBuffers(posvbo);
+        glDeleteBuffers(colvbo);
+        glDeleteBuffers(indvbo);
+
+        // Delete the VAO
+        glBindVertexArray(0);
+        glDeleteVertexArrays(vao);
+	}
+}
