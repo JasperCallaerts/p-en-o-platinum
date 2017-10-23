28f5abefb6bdccfecad1e4d1b0f04587c2ea4e35
Jasper <jasper.snoeks@kuleuven.be>
2017-10-24 00:10:50 +0200

    mouse class :)

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import gui.Window;
+
+public class Main {
+
+	public static void main(String[] args) {
+		new Window().run();
+	}
+
+}
---
+++ src/gui/Mouse.java
@@ -0,0 +1,49 @@
+package gui;
+
+import static org.lwjgl.glfw.GLFW.glfwGetCursorPos;
+
+import java.nio.DoubleBuffer;
+
+import org.lwjgl.BufferUtils;
+
+public class Mouse {
+	
+	private float x;
+	private float y;
+	private float prevX;
+	private float prevY;
+	
+	public Mouse(long window) {
+		DoubleBuffer b1 = BufferUtils.createDoubleBuffer(1);
+        DoubleBuffer b2 = BufferUtils.createDoubleBuffer(1);
+        glfwGetCursorPos(window, b1, b2);
+        prevX = (float) b1.get(0);
+        prevY = -(float) b2.get(0);
+	}
+	
+	public void update(long window) {
+		prevX = x;
+		prevY = y;
+		DoubleBuffer b1 = BufferUtils.createDoubleBuffer(1);
+        DoubleBuffer b2 = BufferUtils.createDoubleBuffer(1);
+        glfwGetCursorPos(window, b1, b2);
+        x = (float) b1.get(0);
+        y = -(float) b2.get(0);
+	}
+	
+	public float x() {
+		return this.x;
+	}
+	
+	public float dx() {
+		return this.x - this.prevX;
+	}
+	
+	public float y() {
+		return this.y;
+	}
+	
+	public float dy() {
+		return this.y - this.prevY;
+	}
+}
