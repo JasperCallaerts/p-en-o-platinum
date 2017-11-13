c9c13872afad4466410ae9e3ffa34fcd15b2137e
MartijnS1997 <martijn.sauwens@gmail.com>
2017-11-13 14:42:56 +0100

    physics engine extracted

--- src/internal/DroneBuilder.java
+++ src/internal/DroneBuilder.java
@@ -26,7 +26,7 @@ public class DroneBuilder {
     public final static Vector RIGHTWING_POS = new Vector(4.0f, 0.0f, 0.0f);
     public final static Vector STABILIZE_POS = new Vector(0.0f, 0.0f, 8.0f);
     public final static Vector STARTPOS = new Vector();
-    public final static Vector START_VEL = new Vector();
+    public final static Vector START_VEL = new Vector(0,0,-6.32f);
     public final static Vector START_ORIENTATION = new Vector();
     public final static Vector START_ROTATION = new Vector();
 
@@ -37,21 +37,21 @@ public class DroneBuilder {
 
 
     public Drone createDrone() {
-        HorizontalWing rightMain, leftMain, horizontalStabilizer;
-        VerticalWing verticalStabilizer;
+        HorizontalWingPhysX rightMain, leftMain, horizontalStabilizer;
+        VerticalWingPhysX verticalStabilizer;
         Drone drone;
 
-        rightMain = new HorizontalWing(RIGHTWING_POS, LIFT_COEFFICIENT, MAIN_WING_MASS, MAX_ANGLE_OF_ATTACK, MAINWING_START_INCL);
-        leftMain = new HorizontalWing(LEFTWING_POS, LIFT_COEFFICIENT, MAIN_WING_MASS, MAX_ANGLE_OF_ATTACK, MAINWING_START_INCL);
-        horizontalStabilizer = new HorizontalWing(STABILIZE_POS, LIFT_COEFFICIENT_STAB, STABILIZER_MASS, MAX_ANGLE_OF_ATTACK, STABS_START_INCL);
-        verticalStabilizer = new VerticalWing(STABILIZE_POS, LIFT_COEFFICIENT_STAB, STABILIZER_MASS, MAX_ANGLE_OF_ATTACK, STABS_START_INCL);
+        rightMain = new HorizontalWingPhysX(RIGHTWING_POS, LIFT_COEFFICIENT, MAIN_WING_MASS, MAX_ANGLE_OF_ATTACK, MAINWING_START_INCL);
+        leftMain = new HorizontalWingPhysX(LEFTWING_POS, LIFT_COEFFICIENT, MAIN_WING_MASS, MAX_ANGLE_OF_ATTACK, MAINWING_START_INCL);
+        horizontalStabilizer = new HorizontalWingPhysX(STABILIZE_POS, LIFT_COEFFICIENT_STAB, STABILIZER_MASS, MAX_ANGLE_OF_ATTACK, STABS_START_INCL);
+        verticalStabilizer = new VerticalWingPhysX(STABILIZE_POS, LIFT_COEFFICIENT_STAB, STABILIZER_MASS, MAX_ANGLE_OF_ATTACK, STABS_START_INCL);
 
-        drone = new Drone(ENGINE_MASS, MAX_THRUST, STARTPOS, START_VEL, START_ORIENTATION, START_ROTATION, rightMain, leftMain, horizontalStabilizer, verticalStabilizer);
+        drone = new Drone(STARTPOS, START_VEL, START_ORIENTATION, START_ROTATION, createConfig());
 
         // if the drone needs to be balanced, do so (balancing is the act of setting the vertical force to 0
         // and the Z value to 0
         if(this.isBalanced()){
-            balanceDrone(drone);
+            //balanceDrone(drone);
         }
 
         System.out.println("Drone velocity: " + drone.getVelocity());
@@ -89,7 +89,7 @@ public class DroneBuilder {
              */
             @Override
             public float getGravity() {
-                return 0;
+                return 9.81f;
             }
 
             /**
@@ -206,67 +206,6 @@ public class DroneBuilder {
 
     }
 
-    public void balanceDrone(Drone drone){
-        // todo implement such that the drone is balanced on start of the execution.
-        // use interval reduction to find the zero point for the lift, and initialize the thrust to
-        // be equal to the "drag" experienced by the aircraft.
-        float stepsize = 1.0f;
-        float velocity = 0.0f;
-        float firstPositive;
-        boolean positiveLift = false;
-
-        // first get the point where the total external forces are larger than zero
-        while(! positiveLift) {
-            if(drone.getTotalExternalForcesWorld().getyValue() > 0.0f) {
-                positiveLift = true;
-                firstPositive = velocity;
-            }else{
-                velocity += stepsize;
-                drone.setVelocity(new Vector(0, 0, -velocity));
-            }
-        }
-
-        velocity = this.findZero(drone, velocity-stepsize, velocity);
-        System.out.println("returned velocity:" + velocity);
-        drone.setVelocity(new Vector(0,0, -velocity));
-        drone.setThrust(drone.getTotalExternalForcesWorld().getzValue());
-
-    }
-
-    public float findZero(Drone drone, float lowerBound, float upperBound){
-
-        float epsilon = 1E-6f;
-        float nbOfSteps = 10000;
-        float prevYValue = Float.MAX_VALUE;
-        float velocityCenter = upperBound;
-
-        for(int index = 0; index != nbOfSteps; index++){
-            //get the value for this iteration
-            velocityCenter = (lowerBound + upperBound)/2.0f;
-            drone.setVelocity(new Vector(0,0,-velocityCenter));
-            float yNextValue = drone.getTotalExternalForcesWorld().getyValue();
-            //System.out.println(drone.getTotalExternalForcesWorld());
-
-            //check if the precision is good enough
-            if(yNextValue == 0.0f){
-                System.out.println("exit with good approx");
-                return velocityCenter;
-            // if not replace the borders
-            }else{
-                if(yNextValue < 0){
-                    lowerBound = velocityCenter;
-                    prevYValue = yNextValue;
-                }else{
-                    upperBound = velocityCenter;
-                    prevYValue = yNextValue;
-                }
-            }
-        }
-        System.out.println("Exit with bad approx");
-        System.out.println(drone.getTotalExternalForcesWorld());
-        System.out.println("Velocity: " + velocityCenter);
-        return velocityCenter;
-    }
 
 
 
@@ -300,13 +239,13 @@ public class DroneBuilder {
         Vector startOrientation = new Vector(0.0f, 0.0f, 0.0f);
         Vector startRotation = new Vector(0.0f, 0.0f, 0.0f);
 
-        rightMain =new HorizontalWing(rightWingPos, liftCoefficient, mainWingMass, maxAngleOfAttack, mainWingStartIncl);
+        rightMain =new HorizontalWingPhysX(rightWingPos, liftCoefficient, mainWingMass, maxAngleOfAttack, mainWingStartIncl);
 
-        leftMain =new HorizontalWing(leftWingPos, liftCoefficient, mainWingMass, maxAngleOfAttack, mainWingStartIncl);
+        leftMain =new HorizontalWingPhysX(leftWingPos, liftCoefficient, mainWingMass, maxAngleOfAttack, mainWingStartIncl);
 
-        horizontalStabilizer =new HorizontalWing(stabilizerPos, liftCoefficient, stabilizerMass, maxAngleOfAttack,0.0f);
+        horizontalStabilizer =new HorizontalWingPhysX(stabilizerPos, liftCoefficient, stabilizerMass, maxAngleOfAttack,0.0f);
 
-        verticalStabilizer =new VerticalWing(stabilizerPos, liftCoefficient, stabilizerMass, maxAngleOfAttack, stabInclination);
+        verticalStabilizer =new VerticalWingPhysX(stabilizerPos, liftCoefficient, stabilizerMass, maxAngleOfAttack, stabInclination);
 
         drone =new Drone(droneMass, engineMass, maxThrust, startPos, startVelocity, startOrientation, startRotation, rightMain, leftMain, horizontalStabilizer, verticalStabilizer, null);
 
