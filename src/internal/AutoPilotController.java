dd7d586266f88d6b02024937f046abfe774aa9f6
Jasper <jasper.snoeks@kuleuven.be>
2017-11-20 16:40:23 +0100

    stable

--- src/internal/AutoPilotController.java
+++ src/internal/AutoPilotController.java
@@ -118,7 +118,7 @@ public class AutoPilotController {
         //APCamera.loadNextImage(currentInputs.getImage());
         APCamera.loadNewImage(currentInputs.getImage());
         
-        Vector center = APCamera.getCenterOfNCubes(1).scalarMult(2f).vectorSum(APCamera.getCenterOfNCubes(5).scalarMult(1f)).scalarMult(1f);
+        Vector center = APCamera.getCenterOfNCubes(1).scalarMult(2f).vectorSum(APCamera.getCenterOfNCubes(5).scalarMult(1f)).scalarMult(3f);
 //        Vector center = APCamera.getCenterOfNCubes(1);
 //        Vector center = APCamera.getCenterOfNCubes(1);
         float xPosition = center.getxValue();
