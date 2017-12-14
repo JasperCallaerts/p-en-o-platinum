52a10c0502b0f23b61f9f621eeff85f6ff9436e3
MartijnS1997 <martijn.sauwens@gmail.com>
2017-12-14 08:50:42 +0100

    split controller

--- src/internal/BetaController.java
+++ src/internal/BetaController.java
@@ -133,8 +133,8 @@ public class BetaController extends AutoPilotController{
         return yPID;
     }
 
-    private PIDController xPID = new PIDController(1.f, 0.0f, 0.1f);
-    private PIDController yPID = new PIDController(1.f, 0.0f, 0.3f);
+    private PIDController xPID = new PIDController(1.f, 0.1f, 0.1f);
+    private PIDController yPID = new PIDController(1.f, 0.1f, 0.3f);
     private PIDController rollPID = new PIDController(1f, 0.0f, 0.0f);
 
 
@@ -155,7 +155,7 @@ public class BetaController extends AutoPilotController{
     private static final float MAX_CUBE_COEFF = 3f;
     public  static final float STABILIZER_STABLE_INCLINATION = 0.0f;
     private static final float GRAVITY = 9.81f;
-    private static final float ROLL_THRESHOLD = (float) (PI * 35.0f/180.0f);
+    private static final float ROLL_THRESHOLD = (float) (PI * 15.0f/180.0f);
     private static final float RAD2DEGREE = (float) (180f/ PI);
     private static final float CHECK_INTERVAL = 1/20.f;
     private static final float X_THRESHOLD = 0f;
